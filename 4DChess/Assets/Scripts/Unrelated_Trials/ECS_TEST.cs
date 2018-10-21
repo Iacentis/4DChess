2eff10ef9d3ef55bf94c978928ef74bf8602559f
Caan <CarlJFandersson@gmail.com>
2018-10-21 19:14:15 +0200

    Initial Commit
    Created a mostly empty Unity Project with proper settings

---
+++ 4DChess/Assets/Scripts/Unrelated_Trials/ECS_TEST.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Transforms;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine.UI;
+
+public class ECS_TEST : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
